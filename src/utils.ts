cce0a5adb5811362fe4fae7b592b350432c7572b
Shebin John <admin@remedcu.com>
2025-05-02 14:28:02 +0200

    Removed unnecessary field

--- src/utils.ts
+++ src/utils.ts
@@ -52,8 +52,6 @@ export type ExecTransactionParameters = TransactionParametersWithSafe & {
 export interface SafeSignature {
   signer: string
   data: string
-  // a flag to indicate if the signature is a contract signature and the data has to be appended to the dynamic part of signature bytes
-  dynamic?: true
 }
 
 export const EIP712_SAFE_MESSAGE_TYPE = {
